97e6b3767e5ebbfce168f1f64efba8a476772d95
Gonzalo Bahamondez Coffre <gonzalo.bahamondez.c@gmail.com>
2019-08-15 23:51:01 -0400

    Removing unused confs on jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?)$',
-    moduleFileExtensions: [
-        'ts',
-        'js'
-    ]
+    collectCoverage: true
 };
