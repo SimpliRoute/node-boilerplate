3720dc9e6e08545e3b6784bebc193a889af8bab3
Gonzalo Bahamondez Coffre <gonzalo.bahamondez.c@gmail.com>
2019-08-15 22:53:20 -0400

    Removing tslint in favor of eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: [
+        '@typescript-eslint'
+    ],
+    rules: {
+        "no-empty-function": "off",
+        "@typescript-eslint/no-empty-function": "error"
+    },
+    extends: [
+        'plugin:@typescript-eslint/recommended'
+    ]
+};
--- src/sum.test.ts
+++ src/sum.test.ts
@@ -2,8 +2,7 @@ import sum from './sum';
 
 describe('Sum Test', () => {
     it('Must return expected sum of 5 to 1 and 4 input', () => {
-        expect(sum(1, 4)).toBe(5);
-       
+        expect(sum(1, 4)).toBe(5);       
     });
     it('Must return expected sum of 3 to 1 and 2 input', () => {
          expect(sum(1, 2)).toBe(3);
